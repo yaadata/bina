d105a8f0446e6e6af9be794b2c6a89c3030900d7
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-12-12 18:14:16 -0600

    test: use same comment pattern

--- sequence/slice/slice_test.go
+++ sequence/slice/slice_test.go
@@ -11,84 +11,84 @@ import (
 
 func TestSliceFromBuiltin(t *testing.T) {
 	t.Run("Can build", func(t *testing.T) {
-		// [A]rrange - With capacity
+		// ========= [A]rrange =========
 		sequence := slice.NewBuiltinBuilder[int]().
 			Capacity(10).
 			Build()
-		// [A]ssert
+		// ========= [A]ssert =========
 		must.Eq(t, 0, sequence.Len())
 		must.Eq(t, 10, cap(sequence.ToSlice()))
 	})
 
 	t.Run("Can build without capacity", func(t *testing.T) {
-		// [A]rrange
+		// ========= [A]rrange =========
 		sequence := slice.NewBuiltinBuilder[int]().
 			Build()
-		// [A]ssert
+		// ========= [A]ssert =========
 		must.Eq(t, 0, sequence.Len())
 		must.Eq(t, 0, cap(sequence.ToSlice()))
 	})
 
 	t.Run("Can build from items", func(t *testing.T) {
-		// [A]rrange
+		// ========= [A]rrange =========
 		sequence := slice.NewBuiltinBuilder[int]().
 			From(1, 2, 3).
 			Build()
-		// [A]ssert
+		// ========= [A]ssert =========
 		must.Eq(t, 3, sequence.Len())
 		must.Eq(t, []int{1, 2, 3}, sequence.ToSlice())
 	})
 
 	t.Run("Can enumerate", func(t *testing.T) {
-		// [A]rrange
+		// ========= [A]rrange =========
 		original := []int{1, 2, 3}
 		sequence := slice.NewBuiltinBuilder[int]().
 			From(original...).
 			Build()
 
-		// [A]ssert
+		// ========= [A]ssert =========
 		for index, value := range sequence.Enumerate() {
 			must.Eq(t, original[index], value)
 		}
 	})
 
 	t.Run("Can gather all", func(t *testing.T) {
-		// [A]rrange
+		// ========= [A]rrange =========
 		original := []int{1, 2, 3}
 		sequence := slice.NewBuiltinBuilder[int]().
 			From(original...).
 			Build()
-		// [A]ct
+		// ========= [A]ct =========
 		var actual []int
 		for value := range sequence.All() {
 			actual = append(actual, value)
 		}
-		// [A]ssert
+		// ========= [A]ssert =========
 		must.True(t, slices.Equal(original, actual))
 	})
 
 	t.Run("Can Extend", func(t *testing.T) {
-		// [A]rrange
+		// ========= [A]rrange =========
 		sequence := slice.NewBuiltinBuilder[int]().
 			From(1, 2, 3).
 			Build()
-		// [A]ct
+		// ========= [A]ct =========
 		sequence.Extend(4, 5, 6)
-		// [A]ssert
+		// ========= [A]ssert =========
 		must.True(t, slices.Equal([]int{1, 2, 3, 4, 5, 6}, sequence.ToSlice()))
 	})
 
 	t.Run("Can Extend From Sequence", func(t *testing.T) {
-		// [A]rrange
+		// ========= [A]rrange =========
 		sequence := slice.NewBuiltinBuilder[int]().
 			From(1, 2, 3).
 			Build()
 		other := slice.NewBuiltinBuilder[int]().
 			From(4, 5, 6).
 			Build()
-		// [A]ct
+		// ========= [A]ct =========
 		sequence.ExtendFromSequence(other)
-		// [A]ssert
+		// ========= [A]ssert =========
 		must.True(t, slices.Equal([]int{1, 2, 3, 4, 5, 6}, sequence.ToSlice()))
 	})
 
