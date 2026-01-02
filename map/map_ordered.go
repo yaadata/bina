58b7161f76786e027385c9d8625bd5497a170769
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2026-01-02 16:31:52 -0500

    refactor: remove extension / reversed on ordered map

--- map/map_ordered.go
+++ map/map_ordered.go
@@ -1,16 +1,11 @@
 package maps
 
 import (
-	"iter"
-
-	"codeberg.org/yaadata/bina/sequence"
 	. "github.com/yaadata/optionsgo"
 )
 
 type OrderedMap[K any, V any] interface {
 	Map[K, V]
-	ToSequence() sequence.Sequence[K]
 	First() Option[MapEntry[K, V]]
 	Last() Option[MapEntry[K, V]]
-	Reversed() iter.Seq2[K, V]
 }
